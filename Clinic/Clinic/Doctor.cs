42996aca46832f6db2935439086677506c8e3c1a
siqqQ <gprex1@yahoo.com>
2016-06-16 20:44:23 +0300

    DeclareClasses

---
+++ Clinic/Clinic/Doctor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Doctor
+    {
+        private string name;
+        private string phone;
+        private string email;
+        private List<Patient> doctorPatients = new List<Patient>();
+
+        public void addNewPatient(Patient newPatient)
+        {
+            doctorPatients.Add(newPatient);
+        }
+
+        
+    }
+}
---
+++ Clinic/Clinic/Speciality.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Speciality
+    {
+        private string name;
+        private List<Doctor> specialityDoctors = new List<Doctor>();
+        private List<Diseases> specialityDiseases = new List<Diseases>();
+
+
+    }
+}

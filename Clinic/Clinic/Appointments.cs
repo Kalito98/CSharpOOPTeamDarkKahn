adacf58e05e343c37ff68075f7d51172d494c720
Kalito98 <Kaloyan.Yanev98@gmail.com>
2016-06-18 13:51:22 +0300

    Added some basic things to appointments

--- Clinic/Clinic/Appointments.cs
+++ Clinic/Clinic/Appointments.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,39 @@ namespace ConsoleApplication2
 {
     class Appointments
     {
+        private DateTime plannedDateAndTime;
+        private Patient patient;
+        private string doctor;          //Replace after class Doctor 
+        private Status status;
+        private int plannedTime;        //in minutes 
+
+        public Appointments(Patient patient, string doctor, Status status, int plannedTime, string time, string date)
+        {
+            this.patient = patient;
+            this.doctor = doctor;
+            this.status = status;
+            this.plannedTime = plannedTime;
+            this.plannedDateAndTime = FormatDate(date, time);
+        }
+
+        public DateTime PlannedDateAndTime
+        {
+            get { return this.plannedDateAndTime; }
+        }
+
+        private DateTime FormatDate(string date, string time)
+        {
+            string input = time + " " + date;
+            string formatDate = "HH:mm yyyy.MM.dd";
+            DateTime returnDate = DateTime.ParseExact(input, formatDate, CultureInfo.InvariantCulture);
+            return returnDate;
+        }
+
+        public enum Status
+        {
+            Planned = 0,
+            Completed = 1,
+            Canceled = 2
+        }
     }
 }

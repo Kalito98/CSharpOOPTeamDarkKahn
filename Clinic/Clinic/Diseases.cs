ef75c724bd4fbaa77c356eeca3d8ddd3769cb691
composer2 <composer2be@gmail.com>
2016-06-19 18:57:04 +0300

    changes in Program.cs add Payment folder add some code in Diseases add Clinic pdf
    Signed-off-by: composer2 <composer2be@gmail.com>

--- Clinic/Clinic/Diseases.cs
+++ Clinic/Clinic/Diseases.cs
@@ -2,5 +2,23 @@
 {
     public class Diseases
     {
+        private string diseasesName;
+
+        public Diseases(string diseasesName)
+        {
+            this.diseasesName= diseasesName;
+        }
+
+        public string DiseasesName
+        {
+            get
+            {
+                return this.diseasesName;
+            }
+            private set
+            {
+                this.diseasesName = value;
+            }
+        }
     }
 }
--- Clinic/Clinic/Program.cs
+++ Clinic/Clinic/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Payment;
+using System;
 
 namespace Clinic
 {
@@ -6,12 +7,22 @@ namespace Clinic
     {
         static void Main(string[] args)
         {
-           //  Some testing for the appointments
+            //  Some testing for the appointments
             // Doctor testDoctor = new Doctor("Cuki", "69696969", "test@abv.bg");
             // Patient testPatient = new Patient();
             // Appointments test = new Appointments("69", testPatient, testDoctor, "Planned", 45, "16:40", "2016.08.19");
             // DateTime testDateTime = test.PlannedDateAndTime;
             // test.GetApointmentInfo();
+            //var payment=new Payments(testDoctor,testPatient,testDiseases, testDiseases.diseasesKind)
+            //var payments = new List<Payments>()
+            //{
+            //    new Payments(testDoctor,testPatient,testDiseases, testDiseases.diseasesKind),
+            //    new Payments(testDoctor1,testPatient1,testDiseases1, testDiseases1.diseasesKind),
+            //    new Payments(testDoctor2,testPatient2,testDiseases2, testDiseases2.diseasesKind),
+            //    new Payments(testDoctor3,testPatient3,testDiseases3, testDiseases3.diseasesKind),
+            //    new Payments(testDoctor4,testPatient4,testDiseases4, testDiseases4.diseasesKind)
+            //};
+            //Payments.PrintPayments(payments);
         }
     }
 }

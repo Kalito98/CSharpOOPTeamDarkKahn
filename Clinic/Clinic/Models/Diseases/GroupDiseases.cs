00d111c101937c66e9dca4d628553f7cb9fa6a01
composer2 <composer2be@gmail.com>
2016-06-21 10:18:28 +0300

    code from Nina on Diseases and also GroupDiseases

---
+++ Clinic/Clinic/Models/Diseases/GroupDiseases.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clinic
+{
+
+    public enum GroupDiseases
+    {
+        ЕстетичнаСтоматология = 0,
+        Пародонтология = 1,
+        Ортодонтия = 2,
+        ХирургичнаСтоматология = 3,
+        Други = 4
+    }
+}
